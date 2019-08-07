60356aaca978d951360bc66d62748887e2eb7e49
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-08-07 17:29:15 -0300

    Added linters and formaters

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+  return <div>Hello World</div>
 }
 
-export default App;
+export default App
