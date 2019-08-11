f4e2efbe1fef5e9c504e71a292abc58bfc44c112
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-08-11 16:02:01 -0300

    Improved NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
+import Box from '../../components/Box'
+import Text from '../../components/Text'
 
 const NotFound: React.FC<RouteComponentProps> = () => (
-  <div>404 Not found...</div>
+  <Box center width="100vw" height="100vh">
+    <Text large bold>
+      404 Not found...
+    </Text>
+  </Box>
 )
 
 export default NotFound
