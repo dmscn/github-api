f114bffedd84f5f177bca2d4363b21b79b9d45a4
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-08-11 16:01:43 -0300

    Refactored Text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import styled, { StyledComponentProps } from 'styled-components'
 
-export type TextProps = StyledComponentProps<'span', any, {}, never>
+export interface TextProps
+  extends StyledComponentProps<'span', any, {}, never> {
+  large?: boolean
+  bold?: boolean
+}
 
-const Text: React.FC<TextProps> = styled.span`
+const Text: React.FC<TextProps> = styled.span<TextProps>`
   color: dimgray;
   text-decoration: none;
   line-height: 2em;
+  font-size: ${props => (props.large ? '52px' : '16px')};
+  font-weight: ${props => (props.bold ? 'bold' : 'normal')};
 `
 
 export default Text
