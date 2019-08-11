6425d8ec26d0fcc28a7774c47059beb78c15c411
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-08-11 15:32:07 -0300

    Styled List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -5,6 +5,35 @@ export type ListProps = StyledComponentProps<'ul', any, {}, never>
 
 const List: React.FC<ListProps> = styled.ul`
   list-style-type: none;
+  border-radius: 3px;
+  background-color: whitesmoke;
+  width: 350px;
+  max-height: 65vh;
+  overflow-y: auto;
+  margin: 16px 0;
+  padding: 0;
+`
+
+export const ListItem: React.FC<ListProps> = styled.li`
+  padding: 16px;
+  color: dimgray;
+  text-decoration: none;
+  cursor: pointer;
+  letter-spacing: 0.5px;
+  line-height: 1.5em;
+
+  &:hover {
+    background-color: gainsboro;
+    font-weight: 600;
+  }
+
+  &:visited {
+    color: silver;
+  }
+
+  &:not(:last-child) {
+    border-bottom: 0.5px solid gainsboro;
+  }
 `
 
 export default List
