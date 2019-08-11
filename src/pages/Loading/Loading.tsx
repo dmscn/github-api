ac7aa2f86a8418c282e291ba43269e18f552084b
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-08-11 15:56:47 -0300

    Styled Loading page

--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import Box from '../../components/Box'
+import Text from '../../components/Text'
+import LoadingIcon from '../../components/LoadingIcon'
 
-const Loading: React.FC = () => <div>Loading...</div>
+const Loading: React.FC = () => (
+  <Box width="100vw" height="100vh" center column>
+    <Text>Loading...</Text>
+    <Box marginVertical={10}>
+      <LoadingIcon />
+    </Box>
+  </Box>
+)
 
 export default Loading
